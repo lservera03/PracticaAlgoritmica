9c8a200834427e8a48d805cccdb3d364d06e49dd
luisservera <lservera03@gmail.com>
2022-12-24 11:50:36 +0100

    Finished ShipReader

--- src/persistence/ShipReader.java
+++ src/persistence/ShipReader.java
@@ -1,6 +1,7 @@
 package persistence;
 
 import business.Ship;
+import business.ShipType;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -9,7 +10,7 @@ import java.util.ArrayList;
 
 public class ShipReader {
 
-    private static final String SHIP_ROUTE = "files/match.csv";
+    private static final String SHIP_ROUTE = "datasets/boats/boatsL.txt";
 
     public ArrayList<Ship> readAllShips(){
         String currentLine;
@@ -20,15 +21,15 @@ public class ShipReader {
         try {
             reader = new BufferedReader(new FileReader(SHIP_ROUTE));
 
-            reader.readLine();
+            reader.readLine(); //Skip first line
 
             while ((currentLine = reader.readLine()) != null) {
-
-                System.out.println(currentLine);
-
                 String[] split = currentLine.split(";");
 
-                //ships.add(new Ship());
+                ships.add(new Ship(Integer.parseInt(split[0]), split[1],
+                        ShipType.getEnumValue(split[2]), Double.parseDouble(split[3]),
+                                Double.parseDouble(split[4]), Integer.parseInt(split[5]),
+                                        Integer.parseInt(split[6]), split[7], Integer.parseInt(split[8]), split[9]));
             }
 
             reader.close();
