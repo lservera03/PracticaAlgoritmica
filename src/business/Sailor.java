88cc66459548d1eedf77edad8dcb441cfe46a3b1
Marc Marzá Osa <marc.marza@students.salle.url.edu>
2022-12-24 12:00:57 +0100

    add function to read to the class sailorReader

--- src/business/Sailor.java
+++ src/business/Sailor.java
@@ -10,4 +10,11 @@ public class Sailor {
     private HashMap<ShipType, Integer> abilities = new HashMap<>();
     private int win_rate;
 
+    public Sailor(int num_membership, String name, double weight, HashMap<ShipType, Integer> abilities, int win_rate) {
+        this.num_membership = num_membership;
+        this.name = name;
+        this.weight = weight;
+        this.abilities = abilities;
+        this.win_rate = win_rate;
+    }
 }
--- src/persistence/SailorReader.java
+++ src/persistence/SailorReader.java
@@ -1,6 +1,48 @@
 package persistence;
 
+import business.Sailor;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class SailorReader {
+
+    private static final String SAILOR_ROUTE = "datasets/sailors/sailorsXS.txt";
+
+    public ArrayList<Sailor> readAllSailors(){
+        String currentLine;
+        BufferedReader reader;
+
+        ArrayList<Sailor> sailors = new ArrayList<>();
+
+        try {
+            reader = new BufferedReader(new FileReader(SAILOR_ROUTE));
+
+            reader.readLine();
+
+            while ((currentLine = reader.readLine()) != null) {
+
+                System.out.println(currentLine);
+
+                String[] split = currentLine.split(";");
+
+                //ships.add(new Ship());
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return sailors;
+    }
+
+
+
 
 
 
