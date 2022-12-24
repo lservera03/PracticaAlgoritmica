9671f34c8192bf935e224c19d0c7555435487a2c
luisservera <lservera03@gmail.com>
2022-12-24 12:12:26 +0100

    Implemented backtracking abstract class.

---
+++ src/business/Backtracking.java
@@ -0,0 +1,20 @@
+package business;
+
+public abstract class Backtracking {
+
+
+    public abstract void run();
+
+    public abstract void prepareLevelTour();
+
+    public abstract boolean isThereSuccessor();
+
+    public abstract void nextBrother();
+
+    public abstract boolean solution();
+
+    public abstract boolean completable();
+
+    public abstract boolean feasible();
+
+}
