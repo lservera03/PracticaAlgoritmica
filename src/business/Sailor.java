7faad4a1201f7e4797a2168167dd87e897820c53
Marc Marzá Osa <marc.marza@students.salle.url.edu>
2022-12-24 11:26:20 +0100

    Create class sailor with atributes

--- src/business/Sailor.java
+++ src/business/Sailor.java
@@ -1,4 +1,12 @@
 package business;
 
+import java.util.HashMap;
+
 public class Sailor {
+    private int num_membership;
+    private String name;
+    private int weight;
+    private HashMap<ShipType, Integer> abilities = new HashMap<>();
+    private int win_rate;
+
 }
