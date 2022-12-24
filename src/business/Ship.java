5ee87e87c164b5f26bad06af28d3395e86f81e30
luisservera <lservera03@gmail.com>
2022-12-24 11:41:11 +0100

    Added datasets, first version of ship reader.

--- src/business/Ship.java
+++ src/business/Ship.java
@@ -14,6 +14,16 @@ public class Ship {
     private String center;
 
 
-
-
+    public Ship(int id, String name, ShipType type, int weight, int length, int capacity, int numCompetitions, String state, int speed, String center) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.weight = weight;
+        this.length = length;
+        this.capacity = capacity;
+        this.numCompetitions = numCompetitions;
+        this.state = state;
+        this.speed = speed;
+        this.center = center;
+    }
 }
--- src/persistence/ShipReader.java
+++ src/persistence/ShipReader.java
@@ -1,10 +1,45 @@
 package persistence;
 
+import business.Ship;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class ShipReader {
 
+    private static final String SHIP_ROUTE = "files/match.csv";
+
+    public ArrayList<Ship> readAllShips(){
+        String currentLine;
+        BufferedReader reader;
+
+        ArrayList<Ship> ships = new ArrayList<>();
+
+        try {
+            reader = new BufferedReader(new FileReader(SHIP_ROUTE));
+
+            reader.readLine();
+
+            while ((currentLine = reader.readLine()) != null) {
+
+                System.out.println(currentLine);
+
+                String[] split = currentLine.split(";");
+
+                //ships.add(new Ship());
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
-
+        return ships;
+    }
 
 
 
