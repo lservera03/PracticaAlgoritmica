45b45727cf8ea79360ab04b8780a931278649520
luisservera <lservera03@gmail.com>
2022-12-24 11:24:04 +0100

    Added Ship and enum business classes.

--- src/business/Ship.java
+++ src/business/Ship.java
@@ -2,7 +2,16 @@ package business;
 
 public class Ship {
 
-
+    private int id;
+    private String name;
+    private ShipType type;
+    private int weight;
+    private int length;
+    private int capacity;
+    private int numCompetitions;
+    private String state;
+    private int speed;
+    private String center;
 
 
 
